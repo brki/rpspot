2254ccd7b6a7baed334c16820dc81eb50d97af3e
Brian King <free.as.in.speech@gmail.com>
2015-02-22 22:13:39 +0100

    allow environ to set DJANGO_SETTINGS_MODULE for manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
+import environ
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rpspot.settings.dev")
+    env = environ.Env(DJANGO_SETTINGS_MODULE=(str, 'rpspot.settings.dev'))
+    root = environ.Path(__file__) - 1
+    env.read_env(root('.env'))
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", env('DJANGO_SETTINGS_MODULE'))
 
     from django.core.management import execute_from_command_line
 
