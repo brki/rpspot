8390edc6fd46349d44b0ac039719bc3609ef0a4f
Brian King <free.as.in.speech@gmail.com>
2015-02-25 14:42:19 +0100

    allow load_playlist command to accept param to determine which source playlist file to use

--- rphistory/management/commands/load_playlist.py
+++ rphistory/management/commands/load_playlist.py
@@ -2,20 +2,33 @@ from django.core.management.base import BaseCommand, CommandError
 from rphistory.radioparadise import (
     get_playlist_from_url, get_playlist_from_file, playlist_to_python, save_songs_and_history)
 from rphistory.models import History
+from rphistory.settings import RP_PLAYLIST_BASE
 
 
 class Command(BaseCommand):
     help = 'Loads new playlist history from radio paradise into database'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--playlist',
+            dest='playlist',
+            default=None,
+            help='Specify which playlist from https://www.radioparadise.com/xml to use (example: --playlist now_4.xml)')
+
     def handle(self, *args, **options):
 
+        playlist_file = options['playlist']
+        if playlist_file:
+            url =  RP_PLAYLIST_BASE + playlist_file
+        else:
+            url = None
         latest_song = History.objects.all().order_by('-played_at').first()
         if latest_song:
             min_time = latest_song.played_at
         else:
             min_time = None
 
-        latest_playlist = get_playlist_from_url()
+        latest_playlist = get_playlist_from_url(url)
         songs = playlist_to_python(latest_playlist, min_time=min_time)
         loaded = save_songs_and_history(songs)
 
--- rphistory/settings.py
+++ rphistory/settings.py
@@ -1,3 +1,4 @@
 from django.conf import settings
 
+RP_PLAYLIST_BASE = getattr(settings, 'RP_PLAYLIST_BASE', 'https://www.radioparadise.com/xml/')
 RP_PLAYLIST_URL = getattr(settings, 'RP_PLAYLIST_URL', "https://www.radioparadise.com/xml/playlist.xml")
