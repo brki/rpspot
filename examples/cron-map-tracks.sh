272f5832ae0a9c12221ddea8cbc46954679b245f
Brian King <free.as.in.speech@gmail.com>
2015-03-03 21:07:22 +0100

    correct example cron syntax

--- examples/cron-map-tracks.sh
+++ examples/cron-map-tracks.sh
@@ -7,6 +7,6 @@ MAX_TRACKS=150
 
 cd $PROJECT_BASE
 echo START $(date) >> $CRON_LOG
-$VENV_PYTHON $PROJECT_BASE/manage.py map_tracks $MAX_TRACKS >> $CRON_LOG 2>&1
+$VENV_PYTHON $PROJECT_BASE/manage.py map_tracks --limit $MAX_TRACKS >> $CRON_LOG 2>&1
 echo END $(date) >> $CRON_LOG
 echo >> $CRON_LOG
