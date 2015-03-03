eb0f51fbbc797be70c559c9acc85e9785c3d5625
Brian King <free.as.in.speech@gmail.com>
2015-03-03 20:11:43 +0100

    Get visitor's country

--- trackmap/views.py
+++ trackmap/views.py
@@ -1,17 +1,34 @@
+from logging import getLogger
+from django.contrib.gis.geoip import GeoIP
 from django.views.generic.base import TemplateView
 from django.utils import timezone
 from dateutil.parser import parse
+from ipware.ip import get_real_ip
 from pytz import utc
 from trackmap.models import Track
+
+
+log = getLogger(__name__)
 
 
 class PlaylistView(TemplateView):
 
     template_name = "trackmap/playlists.html"
 
+    def get_visitor_country(self, request):
+        ip = get_real_ip(self.request)
+        if ip:
+            return GeoIP().country(ip)['country_code']
+        return None
+
     def get_context_data(self, **kwargs):
         context = super(PlaylistView, self).get_context_data(**kwargs)
-        country = 'CH'
+
+        country = self.get_visitor_country(self.request)
+        is_country_known = False
+        if not country:
+            country = 'US'
+            is_country_known = True
 
         start_time = self.request.GET.get('start_time', None)
         if start_time:
@@ -29,4 +46,6 @@ class PlaylistView(TemplateView):
         context['playlist_uri'] = 'spotify:trackset:RadioParadisePlaylist:{}'.format(
             ','.join(track.spotify_id for track in context['tracks'])
         )
+        context['country'] = country
+        context['is_country_known'] = is_country_known
         return context
