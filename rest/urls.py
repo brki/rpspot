a419212badf20f2f998d307df291d1c044e22003
Brian King <brian@liip.ch>
2016-02-17 01:49:10 +0100

    rest: add unmatched song list endpoint

--- rest/urls.py
+++ rest/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from rest import views
+from .views import UnmatchedSongList
 
 
 urlpatterns = [
-    url(r'^history/(?P<country>[A-Z]{2})/$', views.history, name='history')
+    url(r'^history/(?P<country>[A-Z]{2})/$', views.history, name='history'),
+    url(r'^unmatched/(?P<country>[A-Z]{2})/$', UnmatchedSongList.as_view(), name='unmatched'),
 ]
--- rest/views.py
+++ rest/views.py
@@ -1,9 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import ParseError
 from rest_framework.response import Response
+from rest_framework import generics
 from django.utils.dateparse import parse_datetime
 
+from rphistory.models import Song
 from .models import history as json_history
+from .serializers import UnmatchedSongsSerializer
 from .util import utc_now, day_period
 
 
@@ -38,3 +41,25 @@ def extract_datetime_param(request, param_name):
 
     return parsed
 
+
+class UnmatchedSongList(generics.ListAPIView):
+    queryset = Song.objects.all()
+    serializer_class = UnmatchedSongsSerializer
+
+    def list(self, request, country):
+        order = request.query_params.get('order', 'id')
+        if order not in ['id', 'artists__name']:
+            order = 'id'
+
+        queryset = self.get_queryset()
+        queryset = queryset.exclude(pk__in=Song.objects.filter(available_tracks__country=country))
+        queryset = queryset.prefetch_related('artists').select_related('album')
+        queryset = queryset.order_by(order)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
