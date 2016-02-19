4862a6d3e23afbdcf7181ee47f7d06c8bc74b0bf
Brian King <brian@liip.ch>
2016-02-20 00:40:45 +0100

    Add HTML list of unmatched songs

--- rphistory/urls.py
+++ rphistory/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
-from .views import current_datetime
+from .views import unmatched
 
 
 urlpatterns = [
-    url(r'^test/$', current_datetime, name='test'),
+    url(r'^unmatched/(?P<country>[A-Z]{2})/$', unmatched, name='unmatched_html'),
+    url(r'^unmatched/(?P<country>[A-Z]{2})/(?P<page>[0-9]{1,5})/$', unmatched, name='unmatched_page_html'),
 ]
--- rphistory/views.py
+++ rphistory/views.py
@@ -1,12 +1,53 @@
+from urllib.parse import urlencode
 from django.shortcuts import render
-
-# Create your views here.
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-import datetime
+from trackmap import trackmap
+from .models import Song
+
 
 @login_required
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+def unmatched(request, country, page=1):
+    order = request.GET.get('order', None)
+    if order == 'artist':
+        order = 'artists__name'
+    elif order == 'id':
+        order = 'id'
+    else:
+        order = 'played'
+
+    page = max(int(page), 1)
+    page_size = 100
+    start = page_size * (page - 1)
+    end = page_size * page
+
+    track_search = trackmap.TrackSearch()
+    qs = Song.unmatched.in_country(country).artists().album().with_order_by(order)[start:end]
+    songs = []
+    for s in qs:
+        song = []
+        artists = s.artists.all()
+        artist_list = ', '.join(['{} [{}]'.format(a.name, a.id) for a in artists])
+        search_string = 'spotify {} {}'.format(
+            s.corrected_title or s.title,
+            " ".join([a.name for a in artists])
+        )
+        query_string = urlencode({'q': search_string})
+        rp_url = 'https://www.radioparadise.com/rp_2.php?#name=songinfo&song_id={}'.format(s.rp_song_id)
+        spotify_query, _, _, _ = track_search.spotify_query(s)
+
+        song.append({'label': 'Artists', 'value': artist_list, 'type': 'text'})
+        song.append({'label': 'Title', 'value': s.title, 'type': 'text'})
+        if s.corrected_title:
+            song.append({'label': 'Corrected title', 'value': s.corrected_title, 'type': 'text'})
+        song.append({'label': 'Album', 'value': s.album.title, 'type': 'text'})
+        if hasattr(s, 'last_played'):
+            song.append({'label': 'Last played', 'value': s.last_played, 'type': 'text'})
+        song.append({'label': 'RP URL', 'value': rp_url, 'type': 'url'})
+        song.append({'label': 'rp_song_id', 'value': s.rp_song_id, 'type': 'text'})
+        song.append({'label': 'Spotify query', 'value': spotify_query, 'type': 'text'})
+        song.append({'label': 'ASIN', 'value': 'http://www.amazon.com/exec/obidos/ASIN/{}'.format(s.album.asin), 'type': 'url'})
+        song.append({'label': 'Google it', 'value': 'https://google.com/search?{}'.format(query_string), 'type': 'url'})
+
+        songs.append(song)
+
+    return render(request, 'rphistory/unmatched_songs.html', {'songs': songs})
