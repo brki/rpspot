e64085b1b08f41c94466b02f4051bf12b97fda14
Brian King <brian@liip.ch>
2016-02-28 17:27:23 +0100

    trackmap: allow + character in search text

--- trackmap/trackmap.py
+++ trackmap/trackmap.py
@@ -153,7 +153,9 @@ class TrackSearch(object):
 
     # Characters that can be stripped when querying Spotify for a match:
     search_strip_chars_pattern = re.compile('[{}]'.format(
-        re.escape(string.punctuation.replace('-', '').replace('&', '').replace('$', '').replace('#', ''))))
+        re.escape(string.punctuation.replace('-', '') \
+                  .replace('&', '').replace('$', '').replace('#', '').replace('+', '')
+                  )))
 
     # Match things like ", part 2":
     part_x_pattern = re.compile(',? (\((pt\.|part) \d+\)|(pt\.|part \d+))')
