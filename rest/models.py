8d9bb1a5db9413f56e0f135a83ff625f44585542
Brian King <brian@liip.ch>
2016-02-29 20:24:16 +0100

    rest history: allow using base_time and count_vector for history list

--- rest/models.py
+++ rest/models.py
@@ -1,17 +1,7 @@
 from django.db import connection
 
-def history(time_start, time_end, country):
-    """
-    Gets the Radio Paradise song history as a json array, with the associated Spotify
-    track for each song, if any.
 
-    :param datetime time_start:
-    :param datetime time_end:
-    :param string country: two letter country code
-    :return: json string with array of results
-    """
-
-    # TODO cache with a key "country + time_start (minute precision) + time_end (minute precision)"
+def history(l2_where_clause="1", l2_order_by_clause='', l2_limit_clause='', l1_limit_clause='', params=[]):
 
     # Subquery with DISTINCT is used to eliminate some duplicate artist -> track mappings that happen
     # where Radio Paradise changes song artist information and that song is reprocessed by the map_tracks command.
@@ -21,27 +11,38 @@ def history(time_start, time_end, country):
     SELECT DISTINCT ON (played_at) played_at, rp_song_id, title, artist_name, album_title, asin,
                                    spotify_track_id, spotify_album_img_small_url, spotify_album_img_large_url
      FROM (
-        SELECT date_trunc('second', h.played_at) AS played_at, s.rp_song_id,
-               COALESCE(s.corrected_title, s.title) AS title,
-               artist.name as artist_name,
-               album.title as album_title, album.asin,
-               track.spotify_id AS spotify_track_id,
-               spot_album.img_medium_url AS spotify_album_img_small_url, spot_album.img_large_url AS spotify_album_img_large_url,
-               artist.id AS artist_id
-         FROM rphistory_history h
-         JOIN rphistory_song s ON h.song_id = s.id
-         JOIN rphistory_artist_songs ras ON s.id = ras.song_id
-         JOIN rphistory_artist artist ON artist.id = ras.artist_id
-         JOIN rphistory_album album ON album.id = s.album_id
-         LEFT OUTER JOIN trackmap_trackavailability ta ON ta.rp_song_id = s.id AND ta.country = %s
-         LEFT OUTER JOIN trackmap_track track ON track.id = ta.track_id
-         LEFT OUTER JOIN trackmap_album spot_album ON spot_album.id = track.album_id
-         WHERE h.played_at BETWEEN %s AND %s
-     ) subq
+        SELECT * FROM (
+            SELECT date_trunc('second', h.played_at) AS played_at,
+                   s.rp_song_id,
+                   COALESCE(s.corrected_title, s.title) AS title,
+                   artist.name as artist_name,
+                   album.title as album_title,
+                   album.asin,
+                   track.spotify_id AS spotify_track_id,
+                   spot_album.img_medium_url AS spotify_album_img_small_url,
+                   spot_album.img_large_url AS spotify_album_img_large_url,
+                   artist.id AS artist_id
+             FROM rphistory_history h
+             JOIN rphistory_song s ON h.song_id = s.id
+             JOIN rphistory_artist_songs ras ON s.id = ras.song_id
+             JOIN rphistory_artist artist ON artist.id = ras.artist_id
+             JOIN rphistory_album album ON album.id = s.album_id
+             LEFT OUTER JOIN trackmap_trackavailability ta ON ta.rp_song_id = s.id AND ta.country = %s
+             LEFT OUTER JOIN trackmap_track track ON track.id = ta.track_id
+             LEFT OUTER JOIN trackmap_album spot_album ON spot_album.id = track.album_id
+             WHERE {l2_where_clause}
+             {l2_order_by_clause}
+             {l2_limit_clause}
+         ) subq_l2
+         {l1_limit_clause}
+     ) subq_l1
     ORDER BY played_at DESC, artist_id DESC
-    """
-    params = [country, time_start, time_end]
-
+    """.format(
+        l2_where_clause=l2_where_clause,
+        l2_order_by_clause=l2_order_by_clause,
+        l2_limit_clause=l2_limit_clause,
+        l1_limit_clause=l1_limit_clause
+    )
     pretty_print_json = 'true'
     json_sql = "SELECT array_to_json(array_agg(row_to_json(t, {})), {})" \
         " FROM (".format(pretty_print_json, pretty_print_json) + sql + ") t"
@@ -50,6 +51,62 @@ def history(time_start, time_end, country):
     cursor.execute(json_sql, params)
     result = cursor.fetchone()[0]
     return result or '[]'
+
+
+def json_history_count_vector(country, base_time, count_vector):
+    """
+    Gets the Radio Paradise song history as a json array, with the associated Spotify
+    track for each song, if any.
+
+    A positive value for ``count_vector`` will fetch results with a played_at datetime >= base_time,
+    a negative value will fetch results with a played_at datetime <= base_time.
+
+    :param string country: two letter country code
+    :param datetime base_time:
+    :param int count_vector: positive/negative int: how many results to return and in which direction from start_time
+    :return: json string with array of results
+    """
+    if count_vector < 0:
+        comparator = '<='
+        l2_order_by_direction = 'DESC'
+    else:
+        comparator = '>='
+        l2_order_by_direction = 'ASC'
+
+    count = min(200, abs(count_vector))
+
+    l2_where_clause = "h.played_at {} %s".format(comparator)
+    l2_order_clause = "ORDER BY played_at {}".format(l2_order_by_direction)
+    l2_limit_clause = "LIMIT {}".format(count + 30)  # A bit larger than count, to account for possible duplicate values
+    l1_limit_clause = "LIMIT {}".format(count)
+    params = [country, base_time]
+
+    return history(
+        l2_where_clause=l2_where_clause,
+        l2_order_by_clause=l2_order_clause,
+        l2_limit_clause=l2_limit_clause,
+        l1_limit_clause=l1_limit_clause,
+        params=params
+    )
+
+
+def json_history_date_period(country, time_start, time_end):
+    """
+    Gets the Radio Paradise song history as a json array, with the associated Spotify
+    track for each song, if any.
+
+    :param string country: two letter country code
+    :param datetime time_start:
+    :param datetime time_end:
+    :return: json string with array of results
+    """
+    # TODO cache with a key "country + time_start (minute precision) + time_end (minute precision)"
+    l2_where_clause = "h.played_at BETWEEN %s AND %s"
+    params = [country, time_start, time_end]
+    return history(
+        l2_where_clause=l2_where_clause,
+        params=params
+    )
 
 
 def last_24_hours(country):
--- rest/views.py
+++ rest/views.py
@@ -6,40 +6,29 @@ from rest_framework import generics
 from django.utils.dateparse import parse_datetime
 
 from rphistory.models import Song
-from .models import history as json_history
+from .models import json_history_count_vector, json_history_date_period
 from .serializers import UnmatchedSongsSerializer
 from .util import utc_now, get_valid_period
 
 
 @api_view()
 def history(request, country):
-    start_time = extract_datetime_param(request, 'start_time')
-    end_time = extract_datetime_param(request, 'end_time')
-    try:
-        start, end = get_valid_period(start_time, end_time)
-    except ValueError as e:
-        raise ValidationError({'error': str(e)})
+    base_time, count_vector = extract_time_vector(request)
+    if base_time:
+        data = json_history_count_vector(country, base_time, count_vector)
+    else:
+        start_time = extract_datetime_param(request, 'start_time')
+        end_time = extract_datetime_param(request, 'end_time')
+        try:
+            start, end = get_valid_period(start_time, end_time)
+        except ValueError as e:
+            raise ValidationError({'error': str(e)})
 
-    data = json_history(start, end, country)
+        data = json_history_date_period(country, start, end)
+
     response = Response(data)
     response['Content-Length'] = len(data)
     return Response(data)
-
-
-def extract_datetime_param(request, param_name):
-    time = request.query_params.get(param_name, None)
-    if time is None:
-        return None
-
-    parsed = parse_datetime(time)
-    if parsed is None:
-        raise ParseError("Unable to extract datetime value from {} parameter".format(param_name))
-
-    if parsed.tzinfo is None or parsed.tzinfo.utcoffset(parsed) is None:
-        message = "The {} parameter should be specified with a timezone and URL encoded, for example 2016-01-20T16:08:07%2B02:00"
-        raise ParseError(message.format(param_name))
-
-    return parsed
 
 
 class UnmatchedSongList(generics.ListAPIView):
@@ -64,3 +53,43 @@ class UnmatchedSongList(generics.ListAPIView):
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+
+def extract_time_vector(request):
+    """
+    Extract base time and directional count from request, if present.
+
+    A directional count of 50 means get 50 newer items, -50 means get 50 older items.
+    :param request:
+    :return:
+    """
+    base_time = extract_datetime_param(request, 'base_time')
+    if not base_time:
+        return (None, None)
+
+    count_vector = request.query_params.get('count_vector')
+    if count_vector is None:
+        count_vector = 200
+    else:
+        try:
+            count_vector = int(count_vector)
+        except ValueError:
+            raise ValidationError({'error': 'count_vector parameter should be a positive or negative base 10 integer'})
+
+    return (base_time, count_vector)
+
+
+def extract_datetime_param(request, param_name):
+    time = request.query_params.get(param_name, None)
+    if time is None:
+        return None
+
+    parsed = parse_datetime(time)
+    if parsed is None:
+        raise ParseError("Unable to extract datetime value from {} parameter".format(param_name))
+
+    if parsed.tzinfo is None or parsed.tzinfo.utcoffset(parsed) is None:
+        message = "The {} parameter should be specified with a timezone and URL encoded, for example 2016-01-20T16:08:07%2B02:00"
+        raise ParseError(message.format(param_name))
+
+    return parsed
