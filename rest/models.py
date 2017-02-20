9e3c8401e6b0e52767b69bf073f40211a558d9c9
Brian King <free.as.in.speech@gmail.com>
2017-02-20 23:07:03 +0100

    Adapt history json to include tracks from another country, when none are found for searched country

--- rest/models.py
+++ rest/models.py
@@ -1,33 +1,52 @@
 from django.db import connection
 
 
-def history(l2_where_clause="1", l2_order_by_clause='', l2_limit_clause='', l1_limit_clause='', params=[]):
+def history(country,
+            l2_where_clause="1", l2_order_by_clause='', l2_limit_clause='', l2_params=None,
+            l1_limit_clause='', l1_params=None):
+
+    l2_params = l2_params or []
+    l1_params = l1_params or []
+    # Reference country is the one that is used to find tracks, as long as the Spotify API is no longer providing
+    # all the country details when searching, or until another workaround is made.
+    reference_country = 'CH'
+    params = [country, country, reference_country] + l2_params + l1_params
 
     # Subquery with DISTINCT is used to eliminate some duplicate artist -> track mappings that happen
     # where Radio Paradise changes song artist information and that song is reprocessed by the map_tracks command.
     # There are also some duplicate entries for songs that were reported played at, for example, 12:30:00 AND
     # 12:30:00.120999.
     sql = """
-    SELECT DISTINCT ON (played_at) played_at, rp_song_id, title, artist_name, album_title, asin,
-                                   spotify_track_id, spotify_album_img_small_url, spotify_album_img_large_url
+    SELECT DISTINCT ON (played_at)
+      date_trunc('second', played_at_time) AS played_at,
+      rp_song_id,
+      title,
+      artist_name,
+      album_title,
+      asin,
+      spotify_track_id,
+      spotify_album_img_small_url,
+      spotify_album_img_large_url,
+      coalesce(country_match, FALSE) AS country_match
      FROM (
         SELECT * FROM (
-            SELECT date_trunc('second', h.played_at) AS played_at,
+            SELECT h.played_at AS played_at_time,
                    s.rp_song_id,
                    COALESCE(s.corrected_title, s.title) AS title,
-                   artist.name as artist_name,
-                   album.title as album_title,
+                   artist.name AS artist_name,
+                   album.title AS album_title,
                    album.asin,
                    track.spotify_id AS spotify_track_id,
                    spot_album.img_medium_url AS spotify_album_img_small_url,
                    spot_album.img_large_url AS spotify_album_img_large_url,
-                   artist.id AS artist_id
+                   artist.id AS artist_id,
+                   ta.country = %s AS country_match
              FROM rphistory_history h
              JOIN rphistory_song s ON h.song_id = s.id
              JOIN rphistory_artist_songs ras ON s.id = ras.song_id
              JOIN rphistory_artist artist ON artist.id = ras.artist_id
              JOIN rphistory_album album ON album.id = s.album_id
-             LEFT OUTER JOIN trackmap_trackavailability ta ON ta.rp_song_id = s.id AND ta.country = %s
+             LEFT OUTER JOIN trackmap_trackavailability ta ON ta.rp_song_id = s.id AND (ta.country = %s OR ta.country = %s)
              LEFT OUTER JOIN trackmap_track track ON track.id = ta.track_id
              LEFT OUTER JOIN trackmap_album spot_album ON spot_album.id = track.album_id
              WHERE {l2_where_clause}
@@ -36,7 +55,7 @@ def history(l2_where_clause="1", l2_order_by_clause='', l2_limit_clause='', l1_l
          ) subq_l2
          {l1_limit_clause}
      ) subq_l1
-    ORDER BY played_at DESC, artist_id DESC
+    ORDER BY played_at DESC, country_match DESC, artist_id DESC
     """.format(
         l2_where_clause=l2_where_clause,
         l2_order_by_clause=l2_order_by_clause,
@@ -73,20 +92,22 @@ def json_history_count_vector(country, base_time, count_vector):
         comparator = '>='
         l2_order_by_direction = 'ASC'
 
-    count = min(200, abs(count_vector))
+    count = min(400, abs(count_vector))
 
     l2_where_clause = "h.played_at {} %s".format(comparator)
     l2_order_clause = "ORDER BY played_at {}".format(l2_order_by_direction)
-    l2_limit_clause = "LIMIT {}".format(count + 30)  # A bit larger than count, to account for possible duplicate values
+    # The l2_limit is larger than count, to allow for possible duplicate values and songs that are not available
+    # in the region.
+    l2_limit_clause = "LIMIT {}".format(count * 2)
     l1_limit_clause = "LIMIT {}".format(count)
-    params = [country, base_time]
 
     return history(
+        country=country,
         l2_where_clause=l2_where_clause,
         l2_order_by_clause=l2_order_clause,
         l2_limit_clause=l2_limit_clause,
-        l1_limit_clause=l1_limit_clause,
-        params=params
+        l2_params=[base_time],
+        l1_limit_clause=l1_limit_clause
     )
 
 
@@ -101,11 +122,10 @@ def json_history_date_period(country, time_start, time_end):
     :return: json string with array of results
     """
     # TODO cache with a key "country + time_start (minute precision) + time_end (minute precision)"
-    l2_where_clause = "h.played_at BETWEEN %s AND %s"
-    params = [country, time_start, time_end]
     return history(
-        l2_where_clause=l2_where_clause,
-        params=params
+        country=country,
+        l2_where_clause="h.played_at BETWEEN %s AND %s",
+        l2_params=[time_start, time_end]
     )
 
 
